89a941282b07770e9d5cacc0129997bf7c98fb97
Lukas Siemon <lukas.siemon@gmail.com>
2019-03-10 11:38:49 -0700

    amend: removed unnecessary code

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,14 +1,8 @@
 const expect = require('chai').expect;
-const tmp = require('tmp');
 const sfs = require('../src/index');
 
 
 describe('Testing Integration', () => {
-  let dir;
-  beforeEach(() => {
-    dir = tmp.dirSync({ keep: false, unsafeCleanup: true }).name;
-  });
-
   it('Testing Exported Functions', () => {
     expect(Object.keys(sfs)).to.deep.equal(['getExt', 'ensureDir']);
   });
