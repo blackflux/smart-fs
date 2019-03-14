c26f0896ffd84690680f0de8d839d6dd490df664
Lukas Siemon <github@blackflux.com>
2019-03-14 10:29:35 -0700

    amend: spaceSelfClosing now works on file without line breaks

--- src/util/xml-parser.js
+++ src/util/xml-parser.js
@@ -3,10 +3,10 @@ const xmlJs = require('xml-js');
 module.exports.parse = xml => ({
   data: xmlJs.xml2js(xml, { captureSpacesBetweenElements: true }),
   meta: {
-    spaceSelfClosing: xml[xml.search(/.[/?]>\n/g)] === ' '
+    spaceSelfClosing: xml[xml.search(/.[/?]>/g)] === ' '
   }
 });
 module.exports.stringify = obj => `${
   xmlJs.js2xml(obj.data)
-    .replace(/ ?([/?]>\n)/g, obj.meta.spaceSelfClosing ? ' $1' : '$1')
+    .replace(/ ?([/?]>)/g, obj.meta.spaceSelfClosing ? ' $1' : '$1')
 }`;
