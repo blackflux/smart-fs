03c20204839714a841fcdd850d9066563841781a
Lukas Siemon <lukas.siemon@blackflux.com>
2022-04-12 09:46:22 -0700

    amend: minor

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import fs from 'fs';
-import dirname from './logic/dirname.js';
-import filename from './logic/filename.js';
+import fs_ from 'fs';
 
-import guessFile from './logic/guess-file.js';
-import walkDir from './logic/walk-dir.js';
-import cleaningDelete from './logic/cleaning-delete.js';
+import dirname_ from './logic/dirname.js';
+import filename_ from './logic/filename.js';
 
-import smartParse from './logic/smart-parse.js';
-import smartRead from './logic/smart-read.js';
-import smartWrite from './logic/smart-write.js';
+import guessFile_ from './logic/guess-file.js';
+import walkDir_ from './logic/walk-dir.js';
+import cleaningDelete_ from './logic/cleaning-delete.js';
+
+import smartParse_ from './logic/smart-parse.js';
+import smartRead_ from './logic/smart-read.js';
+import smartWrite_ from './logic/smart-write.js';
+
+export const fs = fs_;
+
+export const dirname = dirname_;
+export const filename = filename_;
+
+export const guessFile = guessFile_;
+export const walkDir = walkDir_;
+export const cleaningDelete = cleaningDelete_;
+
+export const smartParse = smartParse_;
+export const smartRead = smartRead_;
+export const smartWrite = smartWrite_;
 
 export default {
-  ...fs,
+  ...fs_,
 
   dirname,
   filename,
